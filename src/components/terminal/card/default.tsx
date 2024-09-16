dda846fe1a504cfac77913e04deef2175c5d9038
Anish-Codeth <codeth690@gmail.comm>
2024-09-16 09:11:56 +0545

    hlep

---
+++ src/components/terminal/card/default.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const HelpDefault = ({ props }) => {
+  return (
+    <div className="mb-2  flex justify-center p-4" onClick={() => props(false)}>
+      <div className=" border-4w-fit">
+        <h1 className="font-bold underline decoration-solid p-3">Tips</h1>
+        <ol className="list-disc list-inside">
+          <li>Click here to continue</li>
+          <li>write help as command for getting list of commands</li>
+          <li>Press enter after writing command for response</li>
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+export default HelpDefault;
---
+++ src/components/terminal/card/help.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Help = () => {
+  return (
+    <div>
+      <div className="mb-2 font-bold">List of commands are:</div>
+      <div className=" list-inside ">
+        <li>contact</li>
+        <li>about</li>
+        <li>help</li>
+        <li>email</li>
+        <li>clear</li>
+      </div>
+      <div className="mt-2 font-bold">Press enter after writing help</div>
+    </div>
+  );
+};
+
+export default Help;
