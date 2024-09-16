80980c0129ac30b58a855649915e95d02b7a38e3
Anish-Codeth <codeth690@gmail.comm>
2024-09-16 22:37:13 +0545

    globally

--- src/components/terminal/page.jsx
+++ src/components/terminal/page.jsx
@@ -8,34 +8,69 @@ import Card from "./card/email";
 import Test from "./test/test";
 import Email from "./card/email";
 import HelpDefault from "./card/default.jsx";
+import { constants } from "buffer";
 
 export default function Terminal() {
-  const [enter, setEnter] = useState(0);
-  const [clear, setClear] = useState(0);
-  const [key,setKey]=useState('') //to show the help
-  const [defaultHelp,setDefaultHelp]=useState(true) 
+  const [enter, setEnter] = useState(0);//to count the number of enter which helps in count also
+  // const [clear, setClear] = useState(0);
+  // const [key,setKey]=useState('') //to show the help
+  // const [defaultHelp,setDefaultHelp]=useState(true) 
+  const [text,setText]=useState([''])
+  
   useEffect(()=>{
-    document.addEventListener('keydown',e=>setKey(e))
-  },[])
-  // Function to update the state
-  const enterFunction = (e) => {
-    if(e==-enter){
-      setDefaultHelp(true)
+    document.addEventListener('keydown',updateText)
+    return ()=>{
+      document.removeEventListener('keydown',updateText)
     }
-    setEnter(enter + e);
+  },[text,enter])
+  // Function to update the state
+  // const enterFunction = (e) => {
+  //   if(e==-enter){
+  //     setDefaultHelp(true)
+  //   }
+  //   setEnter(enter + e);
     
+  // };
+
+  // const setDefaultHelpFunction=(e)=>{
+  //   setDefaultHelp(e)
+  // }
+
+
+  const updateText = (e) => {
+    let temparray=[...text]
+    console.log('dsfdsf',temparray[enter],e.key)
+    let tempText=temparray[enter]
+
+    switch (e.key) {
+      case "Backspace":
+        if(tempText.length==0)
+          return 
+        temparray[enter]=tempText.slice(0,tempText.length-1)
+        break;
+      case "Enter":
+        if (tempText === "clear") {
+          temparray=['']
+        } else {
+          setEnter(enter+1)
+          temparray.push('')
+        }
+        break;
+      default:
+        if (e.key.length === 1) {
+          temparray[enter]=(tempText+e.key)
+        }
+        break;
+    }
+    setText(temparray)
   };
 
-  const setDefaultHelpFunction=(e)=>{
-    setDefaultHelp(e)
-  }
 
   return (
     <div className="max-w-full  bg-black text-white flex flex-col relative min-h-screen ">
       <NavBar />
-      {defaultHelp?<HelpDefault props={setDefaultHelpFunction}/>:
+      {/* {defaultHelp?<HelpDefault props={setDefaultHelpFunction}/>:
       Array.from({ length: enter + 1 }, (_, index) => (
-      index==enter?
         <CommandBlock
           key={index}
           index={index}
@@ -43,15 +78,12 @@ export default function Terminal() {
           token={enter}
           keys={key}
           
-        />:
-        <CommandBlock
-        key={index}
-        index={index}
-        enterFunctionProps={enterFunction}
-        token={enter}
-        
-      />
-      ))}
+        />
+      ))} */}
+      {text.map((t,index)=>{
+       return <CommandBlock key={index} command={t} />
+      })
+    }
     </div>
     // <Email />
 
--- src/components/terminal/textbody/page.jsx
+++ src/components/terminal/textbody/page.jsx
@@ -4,62 +4,60 @@ import responseOfCommand from "./response";
 import About from "../card/about";
 import Card from "../card/page";
 
-const CommandBlock = ({ index, enterFunctionProps, token ,keys}) => {
-  const [text, setText] = useState("");
-  const commandBlockRef = useRef(null); // Ref to manage focus
+const CommandBlock = ({command}) => {
+  // const [text, setText] = useState("");
+  // const commandBlockRef = useRef(null); // Ref to manage focus
   const [response, setResponse] = useState("");
 
 
   // Automatically focus on the command block if it's the current active block
-  useEffect(() => {
-    if (token === index) {
-      commandBlockRef.current.focus();
-    }
-  }, [token, index]);
+  // useEffect(() => {
+  //   if (token === index) {
+  //     commandBlockRef.current.focus();
+  //   }
+  // }, [token, index]);
 
 
 
 
-  const updateText = (e) => {
-    if (index !== token) 
-    {
-      return
-    }
+  // const updateText = (e) => {
+  //   if (index !== token) 
+  //   {
+  //     return
+  //   }
 
-    switch (e.key) {
-      case "Backspace":
-        setText((prevText) => prevText.slice(0, -1));
-        break;
-      case "Enter":
-        if (text === "clear") {
-          enterFunctionProps(-index, 1); // Clear the current block
-        } else {
-          setResponse(<Card title={text} />);
-          enterFunctionProps(1, 0); // Move to the next block
-        }
-        break;
-      default:
-        if (e.key.length === 1) {
-          setText((prevText) => prevText + e.key);
-        }
-        break;
-    }
-  };
+  //   switch (e.key) {
+  //     case "Backspace":
+  //       setText((prevText) => prevText.slice(0, -1));
+  //       break;
+  //     case "Enter":
+  //       if (text === "clear") {
+  //         enterFunctionProps(-index, 1); // Clear the current block
+  //       } else {
+  //         setResponse(<Card title={text} />);
+  //         enterFunctionProps(1, 0); // Move to the next block
+  //       }
+  //       break;
+  //     default:
+  //       if (e.key.length === 1) {
+  //         setText((prevText) => prevText + e.key);
+  //       }
+  //       break;
+  //   }
+  // };
+ 
 
   return (
    
     <div
       className="focus:outline-none p-2"
-      tabIndex={0}
-      ref={commandBlockRef}
-      onKeyDown={updateText}
     >
       <p className="text-green-500 font-bold  mr-4 float-left">
         anishsubedi@pop-os:~$
       </p>
-      <p className=" ">{text}</p>
+      <p className=" ">{command}</p>
 
-      {response}
+      {/* {response} */}
     </div>
   );
 };
